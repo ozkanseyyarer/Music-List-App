5946a176db1a86f77811651745351049180bc6b1
özkan seyyarer <ozkanseyyarer@gmail.com>
2024-03-20 17:46:55 +0300

    Liste içerisinde filtreleme özelliği eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import { FlatList, StyleSheet, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import music_data from './music-data.json'
 import SongCard from './components/SongCard/SongCard'
+import SearchBar from './components/SearchBar'
 
 export default function App() {
   const renderSong=({item}) => <SongCard song={item}/>
+
   const renderSeperator= ()=> <View style={styles.seperator} />
+
+  const handleSearch = text =>{
+    const filteredList = music_data.filter(song=>
+      {
+      const searcedText = text.toLowerCase()
+      const currentTittle = song.title.toLowerCase()
+
+      return currentTittle.indexOf(searcedText)> -1;
+
+    })
+
+    setList(filteredList)
+  };
+
+  const[list,setList]=useState(music_data);
   return (
     <View style={styles.container}>
+      <SearchBar onSearch={handleSearch}/>
       <FlatList  style={styles.container}
       keyExtractor={(item) =>item.id}
-      data={music_data}
+      data={list}
       renderItem={renderSong}
 
       //her bir itemin arasına girecek componenti burada bize hazır verilen component içerisine yazıyoruz
---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+
+    container:{
+        
+        backgroundColor:"#eceff1",
+        padding:1,
+        margin:5,
+        borderRadius:5
+    }
+
+
+
+})
